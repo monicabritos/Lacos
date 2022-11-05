9434f2bbbdf555db9c54a7a8e6e92e3941551a74
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-11-04 22:23:12 -0300

    resulução

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿int numeroDeExibições = 0;
+int vezesAExibir = 25;
+
+while(numeroDeExibições<vezesAExibir)
+{
+Console.Write("FELIZ NATAL, HOHOHOOOO");
+
+numeroDeExibições++;
+}
+Console.WriteLine(numeroDeExibições);
